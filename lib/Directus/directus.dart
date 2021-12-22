6e3cfc5688b3f4b084ef406206d2706a1d96fead
JancoSOUS <jancobuitendag@gmail.com>
2021-12-22 14:00:34 +0200

    Removed unnecessary code

--- lib/Directus/directus.dart
+++ lib/Directus/directus.dart
@@ -1,12 +1,6 @@
-import 'dart:io';
-
 import 'package:directus/directus.dart';
 
 Future<DirectusResponse> sendName(String name) async {
-  String domain = 'dinedash.ddns.net';
-  var internetAddress = await InternetAddress.lookup(domain);
-  String ip = 'http://${internetAddress.elementAt(0).address}:80';
-
-  final sdk = await Directus(ip).init();
+  final sdk = await Directus('http://dinedash.ddns.net').init();
   return await sdk.items('users').createOne({'name': name});
 }
